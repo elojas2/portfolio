9ca4c23a8b6792a67a4220d664e1d9aa5638d622
eloyse.fernanda <eloyse.fernanda@g.globo>
2024-04-07 15:37:51 -0300

    subindo alteracoes pequenas

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,30 +1,37 @@
-import { Grid, styled } from "@mui/material"
+import { Button, Container, Grid, Typography, styled } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.jpg"
-
+import DownloadIcon from "@mui/icons-material/Download"
 const Hero = () => {
 
     const StyledHero = styled('div')(()=> ({
-        background: 'black'
+        background: 'black',
+        height: "100vh",
     }))
     const StyledImg = styled('img')(()=> ({
-        width: "60%",
+        width: "100%",
         borderRadius: "50%"
 
     }))
     return (
         <>
-            <StyledHero>
-                <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <StyledImg src={Avatar} />
+            <Container>
+                <StyledHero>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                            <StyledImg src={Avatar} />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography color="primary" variant="h1"> Eloyse Fernanda </Typography> 
+                            <Typography color="primary" variant="h2"> I'm a Software Enginer </Typography> 
+                            <Button>  
+                                <DownloadIcon/> 
+                                Download CV 
+                            </Button>
+                            <Button> Contact me </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={8}>
-                        aaaa
-                    </Grid>
-                </Grid>
-                Hero
-
-            </StyledHero>
+                </StyledHero>
+            </Container>
         </>
     )
   }
